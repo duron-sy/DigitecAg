6a3970ec351b1a226a7c36ffa809c4072eef0a68
syukisihor <shizalrone@gmail.com>
2024-08-17 09:23:03 -0600

    Update Histogram update event

--- app/static/app/js/components/Histogram.jsx
+++ app/static/app/js/components/Histogram.jsx
@@ -264,31 +264,31 @@ export default class Histogram extends React.Component {
   handleUnitSystemChanged = e => {
     this.redraw();
     this.setState({
-        minInput: this.props.unitForward(this.state.min).toFixed(6), 
-        maxInput: this.props.unitForward(this.state.max).toFixed(6)
+        minInput: this.props.unitForward(this.state.min).toFixed(8), 
+        maxInput: this.props.unitForward(this.state.max).toFixed(8)
     });
   }
     
-  componentDidUpdate(prevProps, prevState){
-      if (prevState.min !== this.state.min || prevState.max !== this.state.max){
+componentDidUpdate(prevProps, prevState){
+    if (prevState.min !== this.state.min || prevState.max !== this.state.max){
         this.setState({
-            minInput: this.props.unitForward(this.state.min).toFixed(6), 
-            maxInput: this.props.unitForward(this.state.max).toFixed(6)
+            minInput: this.props.unitForward(this.state.min).toFixed(3), 
+            maxInput: this.props.unitForward(this.state.max).toFixed(3)
         });
-      }
+    }
 
-      if (prevState.min !== this.state.min || 
-          prevState.max !== this.state.max ||
-          prevProps.colorMap !== this.props.colorMap ||
-          prevProps.statistics !== this.props.statistics){
+    if (prevState.min !== this.state.min || 
+        prevState.max !== this.state.max ||
+        prevProps.colorMap !== this.props.colorMap ||
+        prevProps.statistics !== this.props.statistics){
 
-          if (prevProps.statistics !== this.props.statistics) this.reset();
-          if (!this.maxDown && !this.minDown) this.redraw();
-          this.updateColorMap(prevProps.colorMap !== this.props.colorMap);
-          
-          if (this.props.onUpdate !== undefined) this.props.onUpdate({min: this.state.min, max: this.state.max});
-      }
-  }
+        if (prevProps.statistics !== this.props.statistics) this.reset();
+        if (!this.maxDown && !this.minDown) this.redraw();
+        this.updateColorMap(prevProps.colorMap !== this.props.colorMap);
+        
+        if (this.props.onUpdate !== undefined) this.props.onUpdate({min: this.state.min.toFixed(8), max: this.state.max.toFixed(8)});
+    }
+}
     
   updateColorMap = (recreate) => {
     if (!this.colorMapElem) return;
